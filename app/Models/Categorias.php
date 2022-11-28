ed05a9f64a9a1edbb5b1bd604eb20ba028df288e
JosemaDAW <josemanuelgallurt24@gmail.com>
2022-11-28 12:40:02 +0100

    FK productos/categorias

--- app/Models/Categorias.php
+++ app/Models/Categorias.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categorias extends Model
 {
+    public function productos() {
+        return $this->hasMany('App\ModelsProductos');
+        }
     use HasFactory;
 }
--- app/Models/Productos.php
+++ app/Models/Productos.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Productos extends Model
 {
+    public function categoria() {
+        return $this->belongsTo('App\Models\Categorias');
+        }
     use HasFactory;
 }
