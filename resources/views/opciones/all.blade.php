608d02350c9c2895791a943502ffe55eb8cd8209
Juan B <juan.baronviciana@gmail.com>
2022-11-30 20:38:48 +0100

    añado confirmacion de borrado a todas las tablas

--- resources/views/opciones/all.blade.php
+++ resources/views/opciones/all.blade.php
@@ -17,7 +17,7 @@
                 <form action = "{{route('opciones.destroy', $opcion->id)}}" method="POST">
                     @csrf
                     @method("DELETE")
-                    <input class="btn btn-outline-danger" type="submit" value="Borrar">
+                    <input class="btn btn-outline-danger" type="submit" value="Borrar" onclick='destroy(event)'>
                 </form>
             </td>
             
@@ -25,3 +25,12 @@
     </table>
     <a class ="btn btn-outline-success" href="{{ route('opciones.create') }}">Nuevo</a>
 @endsection
+
+<script type = "text/javascript">
+  function destroy(e){
+    if (!confirm('¿Seguro que desea borrar este recurso?')){
+    e.preventDefault();
+    }
+
+  }
+  </script>
