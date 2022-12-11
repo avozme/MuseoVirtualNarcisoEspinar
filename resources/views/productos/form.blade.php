e5c6e3321dc798b23c061dc29127d8b416dd60de
JosemaDAW <josemanuelgallurt24@gmail.com>
2022-12-11 14:34:08 +0100

    agregar campos funciona

--- resources/views/productos/form.blade.php
+++ resources/views/productos/form.blade.php
@@ -6,13 +6,13 @@
 
 @section("content")
     @isset($producto)
-        <form  action="{{ route('productos.update', ['producto' => $producto->id]) }}" method="POST">
+        <form  action="{{ route('productos.update', ['producto' => $producto->id]) }}" method="POST" id="formulario">
         @method("PUT")
     @else
     <form action="{{ route('productos.store') }}" method="POST" id="formulario">
     @endisset
         @csrf
-        <div class="container-fluid">
+        <div class="container-fluid" id="miFormulario">
             Nombre del producto:<input class="form-control" type="text" name="name" value="{{$producto->name ?? '' }}"><br>
             Descripción:<input class="form-control" type="text" name="description" value="{{$producto->description ?? '' }}"><br>
             Dimensiones:<input class="form-control" type="text" name="dimensions" value="{{$producto->dimensions ?? '' }}"><br>
@@ -20,13 +20,11 @@
             Técnica:<input class="form-control" type="text" name="technique" value="{{$producto->technique ?? '' }}"><br>
             Imagen:<input class="form-control" type="text" name="image" value="{{$producto->image ?? '' }}"><br>
             Categoria:<select class="form-select" type="text" name="categoria_id" id="categoria_id" onchange="actualizar_items()">
-
             @foreach ($categoriasList as $categoria) {
                 <option value='{{$categoria->id}}'>{{$categoria->name}}</option>
             @endforeach
-
-        </select>
-            <input class="btn btn-dark center mt-3" type="submit" value="Enviar">    
+            </select>
+            <input class="btn btn-dark center mt-3" type="submit" value="Enviar" id="submitButton">    
         </div>
     </form>
 @endsection
@@ -38,13 +36,19 @@
         id_categoria = document.getElementById("categoria_id").value;
 
         //let lista_items = null;
-        fetch("/categorias/get_items/" + id_categoria).then(data=> data.json()).then(json=> {        
-                /*let lista_items = */
-                console.log(json);
+        fetch("/categorias/get_items/" + id_categoria).then(data=> data.json()).then(json=> {         
                 json.forEach(item => {
-                    formulario.append("<input class='form-control' type='text' name='"+item.name+"'>" );
+                    var input = document.createElement("input");
+                    
+                    input.type = "text";
+                    input.name = item.name;
+                    input.placeholder = item.name;
+                    var form = document.getElementById("miFormulario");
+                    input.classList.add("form-control");
+                    input.classList.add("mt-4");
+                    form.appendChild(input);
                     console.log(item);
                 });
             })
-        }      
+        }  
 </script>
