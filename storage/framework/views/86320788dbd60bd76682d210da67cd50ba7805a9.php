b48777f0b8c56c7deab1b1b260eaccfa5f51430a
Juan B <juan.baronviciana@gmail.com>
2023-01-02 14:06:33 +0100

    subida de imagenes

---
+++ storage/framework/views/86320788dbd60bd76682d210da67cd50ba7805a9.php
@@ -0,0 +1,45 @@
+
+
+<?php $__env->startSection("title", "Administración de imágenes"); ?>
+
+<?php $__env->startSection("header", "Administración de imágenes"); ?>
+
+<?php $__env->startSection("content"); ?>
+    
+<table class="table table-hover">
+    <tr>
+      <th scope="col">Imagen</th>
+      <th scope="col">Producto</th>
+      <th scope="col"></th> 
+      <th scope="col"></th> 
+    </tr>
+    <?php $__currentLoopData = $imagenesList; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $imagene): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+        <tr>
+            <td><?php echo e($imagene->image); ?></td>
+            <td><?php echo e($imagene->producto->name); ?></td>
+            <td>
+                <a class="btn btn-outline-secondary" href="<?php echo e(route('imagenes.edit', $imagene->id)); ?>">Modificar</a></td>
+            <td>
+                <form action = "<?php echo e(route('imagenes.destroy', $imagene->id)); ?>" method="POST">
+                    <?php echo csrf_field(); ?>
+                    <?php echo method_field("DELETE"); ?>
+                    <input class="btn btn-outline-danger"  type="submit" value="Borrar" onclick='destroy(event)'>
+                </form>
+            </td>
+
+    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    </table>
+    <div class ="d-grid gap-4 d-md-flex justify-content-md-start ms-2">
+      <a class ="btn btn-outline-success" href="<?php echo e(route('imagenes.create')); ?>">Nuevo</a>
+    </div>
+<?php $__env->stopSection(); ?>
+
+<script type = "text/javascript">
+  function destroy(e){
+    if (!confirm('¿Seguro que desea borrar este recurso?')){
+    e.preventDefault();
+    }
+
+  }
+  </script>
+<?php echo $__env->make("layouts.master", \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/html/resources/views/imagenes/all.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/ef221e94f90f0e8f7f198438d10936a48b96b633.php
@@ -0,0 +1,60 @@
+
+
+<?php $__env->startSection("title", "Administración de productos"); ?>
+
+<?php $__env->startSection("header", "Administración de productos"); ?>
+
+<?php $__env->startSection("content"); ?>
+<table class="table table-hover">
+<tr>
+      <th scope="col">Nombre</th>
+      <th scope="col">Descripción</th> 
+      <th scope="col">Dimensiones</th>
+      <th scope="col">Colección</th> 
+      <th scope="col">Técnica</th>
+      <th scope="col">Imagen</th>    
+      <th scope="col">Categoría</th>
+      <th scope="col"></th>  
+      <th scope="col"></th> 
+      <th scope="col"></th>  
+      <th scope="col"></th> 
+    </tr>
+    <?php $__currentLoopData = $productosList; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $producto): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+        <tr>
+            <td><?php echo e($producto->name); ?></td>
+            <td><?php echo e($producto->description); ?></td>
+            <td><?php echo e($producto->dimensions); ?></td>
+            <td><?php echo e($producto->collection); ?></td>
+            <td><?php echo e($producto->technique); ?></td>
+            <td><?php echo e($producto->image); ?></td>
+            <td><?php echo e($producto->categoria->name); ?></td>
+
+
+            <td>
+                <a class="btn btn-outline-secondary" href="<?php echo e(route('productos.edit', $producto->id)); ?>">Modificar</a></td>
+            <td> <!-- anadir ruta para ver producto completo -->
+                <a class="btn btn-outline-secondary" href="<?php echo e(route('productos.show', $producto->id)); ?>">Ver más</a></td>
+            <td>
+                <form action = "<?php echo e(route('productos.destroy', $producto->id)); ?>" method="POST">
+                    <?php echo csrf_field(); ?>
+                    <?php echo method_field("DELETE"); ?>
+                    <input class="btn btn-outline-danger" type="submit" value="Borrar" onclick='destroy(event)'>
+                </form>
+            </td>
+    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    </table>
+    <div class ="d-grid gap-4 d-md-flex justify-content-md-start ms-2">
+    <a class="btn btn-outline-success"  href="<?php echo e(route('productos.create')); ?>">Nuevo</a>
+    </div>
+<?php $__env->stopSection(); ?>
+
+<script type = "text/javascript">
+  function destroy(e){
+    if (!confirm('¿Seguro que desea borrar este recurso?')){
+    e.preventDefault();
+    }
+
+  }
+  </script>
+
+<?php echo $__env->make("layouts.master", \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/html/resources/views/productos/all.blade.php ENDPATH**/ ?>
