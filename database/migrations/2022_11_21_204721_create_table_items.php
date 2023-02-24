b0588b16fa9b111fd97638f64760638a21b1ffff
Juan B <juan.baronviciana@gmail.com>
2023-02-24 12:47:29 +0100

    cambio migraciones

--- database/migrations/2022_11_21_204721_create_table_items.php
+++ database/migrations/2022_11_21_204721_create_table_items.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50);
+            $table->text('name', 65535);
             $table->integer('categoria_id');
             $table->timestamps();
         });
