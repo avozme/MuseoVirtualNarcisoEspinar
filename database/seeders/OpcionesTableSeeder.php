f559e13c531b75f8abacbf89a5db2b25b165d220
Juan B <juan.baronviciana@gmail.com>
2023-02-24 13:20:18 +0100

    añado opciones a los seeders

--- database/seeders/OpcionesTableSeeder.php
+++ database/seeders/OpcionesTableSeeder.php
@@ -25,6 +25,49 @@ class OpcionesTableSeeder extends Seeder
             'key' => 'logo',
             'type' => 'foto'
         ]);
+        
+        DB::table('opciones')->insert([
+            'value' => 'Un poco más cerca de Álmería',
+            'key' => 'titulo',
+            'type' => 'titulo'
+        ]);
+
+        DB::table('opciones')->insert([
+            'value' => '#ADA191',
+            'key' => 'color_nav',
+            'type' => 'color'
+        ]);
+
+
+        DB::table('opciones')->insert([
+            'value' => '#FFFFFF',
+            'key' => 'color_titulo_subtitulo',
+            'type' => 'color'
+        ]);
+
+        DB::table('opciones')->insert([
+            'value' => '#000000',
+            'key' => 'color_raton_encima_elementos_menu',
+            'type' => 'color'
+        ]);
+
+        DB::table('opciones')->insert([
+            'value' => '#FFFFFF',
+            'key' => 'color_elementos_menu',
+            'type' => 'color'
+        ]);
+
+        DB::table('opciones')->insert([
+            'value' => '#ADA191',
+            'key' => 'paginacion',
+            'type' => 'color'
+        ]);
+
+        DB::table('opciones')->insert([
+            'value' => 'CONOCE SU HISTORIA',
+            'key' => 'subTitulo',
+            'type' => 'subTitulo'
+        ]);
 
     }
 }
