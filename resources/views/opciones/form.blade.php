5ae48d8fb04e9db2c1b34c8d3d4c1110364c0db4
avozme <avozme@yahoo.com>
2023-03-01 14:33:02 +0100

    Añado previsualizador de fuentes en vista opciones/form.blade.php

--- resources/views/opciones/form.blade.php
+++ resources/views/opciones/form.blade.php
@@ -11,6 +11,10 @@
         @method("PUT")
             @csrf
             <div class="container-fluid">
+                <label class="control-label w-100 mt-2">Clave:</label>
+                <input class="form-control mt-2"  type="text" name="key" disabled value="{{$opcion->key ?? '' }}"> <!--Si se pone disabled en el input no se modifiquen los valores -->
+                <label class="control-label w-100 mt-2">Tipo:</label>
+                <input class="form-control mt-2"  type="text" name="type" disabled value="{{$opcion->type ?? '' }}">
                 @if($opcion->type == 'image')
                     <!-- Opción de tipo "image" -->
                     <label class="control-label w-100 mt-2">Foto:</label>
@@ -22,6 +26,25 @@
                     <input class="form-control mt-2" id="value" type="text" name="value" value="{{$opcion->value ?? '' }}">
                     <label class="control-label w-100 mt-2">Color:</label>
                     <input type="color" id="color" onChange="actualizarColor()" name="color" value="{{$opcion->value}}">
+                @elseif($opcion->type == 'font')
+                <!-- Opción de tipo "font" -->
+                    <label class="control-label w-100 mt-2" style="font-family: Montserrat">Valor:</label>
+                    <!-- Lista de fuentes de Google fonts que se pueden usar -->
+                    @php
+                        $fonts = array('Lato', 'Lora', 'Merriweather', 'Montserrat', 'Noto Serif', 'Nunito Sans', 'Open Sans', 'Oswald', 'Playfair Display', 'PT Serif', 'Raleway', 'Roboto', 'Roboto Slab', 'Source Sans Pro', 'Work Sans');
+                    @endphp
+                    @foreach ($fonts as $font)
+                    <link href="https://fonts.googleapis.com/css?family={{$font}}" rel="stylesheet">
+                    @endforeach
+                    <div class="form-control mt-2"> 
+                    <select name="value" id="value" onChange="actualizarTextoEjemplo()">
+                        @foreach ($fonts as $font)
+                            <option value="{{$font}}" id="font_{{$font}}" 
+                            style="font-family: {{$font}}" @if($opcion->value == $font) selected @endif>{{$font}}</option>
+                        @endforeach
+                    </select>
+                    </div>
+                    <div class="form-control" id="textoEjemplo" style="font-family: {{$opcion->value}}; font-size: 140%">Así se verá el texto con la fuente {{$opcion->value}}</div>
                 @elseif($opcion->type == 'longText')
                 <!-- Opción de tipo "longText" -->
                     <label class="control-label w-100 mt-2">Texto:</label>
@@ -31,10 +54,6 @@
                     <label class="control-label w-100 mt-2">Valor:</label>
                     <input class="form-control mt-2" id="value" type="text" name="value" value="{{$opcion->value ?? '' }}">
                 @endif
-                <label class="control-label w-100 mt-2">Clave:</label>
-                <input class="form-control mt-2"  type="text" name="key" disabled value="{{$opcion->key ?? '' }}"> <!--Si se pone disabled en el input no se modifiquen los valores -->
-                <label class="control-label w-100 mt-2">Tipo:</label>
-                <input class="form-control mt-2"  type="text" name="type" disabled value="{{$opcion->type ?? '' }}">
                 <input class="btn btn-dark center mt-2" type="submit" value="Enviar">
             </div>
         </form>
@@ -64,6 +83,11 @@
             function actualizarColor(){
                 color=document.getElementById("color").value;
                 document.getElementById("value").value = color;
+            }
+            function actualizarTextoEjemplo(){
+                font = document.getElementById("value").value;
+                document.getElementById("textoEjemplo").innerHTML = "Así se verá el texto con la fuente " + font;
+                document.getElementById("textoEjemplo").style.fontFamily = font;
             }
         </script>
 
