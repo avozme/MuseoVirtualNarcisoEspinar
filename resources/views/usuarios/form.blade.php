c9f961e09fb3cd505afcafe87bcc33e339e95e41
josemaDAW <josemanuelgallurt24@gmail.com>
2023-03-07 19:24:39 +0100

    instalación componente imagenes

--- resources/views/usuarios/form.blade.php
+++ resources/views/usuarios/form.blade.php
@@ -17,10 +17,10 @@
             Correo:<input class="form-control mb-3" type="email" name="email" value="{{$usuarios->email ?? '' }}">
             <input type="hidden" name="password" value="{{$usuarios->password ?? '' }}">
             Tipo:
-            <select class="form-select mb-3" name='type' >
-                <option @if ($usuarios->type == 'SuperAdmin')  selected @endif value='SuperAdmin'>Super Admin</option>
-                <option @if ($usuarios->type == 'Admin') selected @endif value='Admin'>Admin</option>
-                <option @if ($usuarios->type == 'Basico') selected @endif value='Basico'>Basico</option>
+            <select class="form-select mb-3" name='type'>
+                <option @if (isset($usuarios) && $usuarios->type == 'SuperAdmin')  selected @endif value='SuperAdmin'>Super Admin</option>
+                <option @if (isset($usuarios) && $usuarios->type == 'Admin') selected @endif value='Admin'>Admin</option>
+                <option @if (isset($usuarios) && $usuarios->type == 'Basico') selected @endif value='Basico'>Basico</option>
             </select>
 
 
