2fae27f37ff5f1f55bdc4f2ce42838bdd2599d56
Juan B <juan.baronviciana@gmail.com>
2023-03-24 08:21:32 +0100

    resuelvo issue #66

--- resources/views/front/buscador.blade.php
+++ resources/views/front/buscador.blade.php
@@ -9,14 +9,23 @@
                     value="{{isset($textoBusqueda) ? $textoBusqueda : ''}}">
                 <button class="btn btn-dark" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
             </div>
+            <div class="informacion_busquedas" style="font-family: {{$opciones['tipografia3']}}">
+                La búsqueda se realizará en todos los campos de todas las categorías.<br>
+                El resultado de su búsqueda es una aproximación, para buscar coincidencias exactas existe la posibilidad de buscar el texto entre comillas "".</div>
         </form>
     </div>
     <!--Fin Buscador General-->
-
+    <br>
+    <br>
+    <br>
     <!-- Checkbox categorias -->
     <div class="buscador_por_campos pt-5">
         <form action="{{route('buscadorPorCampos')}}" method="POST" style="font-family: {{$opciones['tipografia3']}}">
             BÚSQUEDA POR CAMPOS: <br>
+            <div class="informacion_busquedas" style="font-family: {{$opciones['tipografia3']}}">
+                El buscador por campos realizará la búsqueda en el/los campo/s deseado/s de la categoría seleccionada.<br>
+                Además, el resultado buscará coincidencias parecidas a su búsqueda.<br>
+                Para buscar coincidencias exactas existe la posibilidad de buscar el texto entre comillas "".</div>
             @csrf
             @foreach ($categoriasList as $key => $categoria)
             <label class="form-check-label mb-3  mt-3"><input @if ($key==0) checked @endif class="form-check-input"
@@ -30,14 +39,14 @@
                 @foreach ($categoriasList as $key => $categoria)
                 <div class="@if ($key != 0) d-none @endif items categoria{{$key}} ">
                     @foreach($categoria->items as $items)
-                    <label class="col-md-4" style="margin-right:7%"> {{$items->name}} 
+                    <label class="col-md-4" style="margin-right:7%"> {{$items->name}}
                         <input class="form-control" type="text" name="items[{{$items->id}}]">
                     </label>
                     @endforeach
                 </div>
                 @endforeach
             </div>
-            <button class="mt-3 btn btn-dark" type="submit">Enviar búsqueda &nbsp;&nbsp;&nbsp;<i
+            <button class="mt-3 btn btn-dark" type="submit">Buscar &nbsp;&nbsp;&nbsp;<i
                     class="fa-solid fa-magnifying-glass"></i></button>
         </form>
     </div>
