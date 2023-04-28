0351a2d2c99f6e8eca75abb4f056bfb985eaeecb
Juan B <juan.baronviciana@gmail.com>
2023-04-28 09:47:19 +0200

    Signos diacríticos

--- resources/views/front/buscador.blade.php
+++ resources/views/front/buscador.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.front')
 @section('content')
 <div class="container" style="margin-top: 120px">
+
     <!-- Buscador General-->
     <div class="w-50 pb-5" id="buscador_general_front">
+        
         <form action="{{route('buscadorFront')}}" method="GET">
             <div class="input-group" style="font-family: {{$opciones['tipografia3']}}">
                 <input type="text" class="form-control" id="texto" name="textoBusqueda" placeholder="Busqueda general"
@@ -11,19 +13,60 @@
             </div>
             <div class="informacion_busquedas" style="font-family: {{$opciones['tipografia3']}}">
                 La búsqueda se realizará en todos los campos de todas las categorías.<br>
-                El resultado de su búsqueda es una aproximación, para buscar coincidencias exactas existe la posibilidad de buscar el texto entre comillas "".</div>
+                El resultado de su búsqueda es una aproximación, para buscar coincidencias exactas existe la posibilidad
+                de buscar el texto entre comillas "".</div>
         </form>
+        <div class="signos_diacriticos">
+        <div style="font-family: Roboto">Signos Diacríticos:</div>
+
+        <table style="font-size:36px">
+            <tr>
+                <td>Ā</td>
+                <td>Ī</td>
+                <td>Ū</td>
+                <td>Š</td>
+                <td>Ŷ</td>
+                <td>Ḥ</td>
+                <td>Ṣ</td>
+                <td>Ḍ</td>
+                <td>Ẓ</td>
+                <td>Ṭ</td>
+                <td>Ḏ</td>
+                <td>Ṯ</td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>ā</td>
+                <td>ī</td>
+                <td>ū</td>
+                <td>š</td>
+                <td>ŷ</td>
+                <td>ḥ</td>
+                <td>ṣ</td>
+                <td>ḍ</td>
+                <td>ẓ</td>
+                <td>ṭ</td>
+                <td>ḏ</td>
+                <td>ṯ</td>
+                <td>ʿ</td>
+                <td>’</td>
+            </tr>
+        </table>
+    </div>
     </div>
     <!--Fin Buscador General-->
     <br>
     <br>
     <br>
     <!-- Checkbox categorias -->
+    
     <div class="buscador_por_campos pt-5">
         <form action="{{route('buscadorPorCampos')}}" method="POST" style="font-family: {{$opciones['tipografia3']}}">
             BÚSQUEDA POR CAMPOS: <br>
             <div class="informacion_busquedas" style="font-family: {{$opciones['tipografia3']}}">
-                El buscador por campos realizará la búsqueda en el/los campo/s deseado/s de la categoría seleccionada (casillas redondas).<br>
+                El buscador por campos realizará la búsqueda en el/los campo/s deseado/s de la categoría seleccionada
+                (casillas redondas).<br>
                 Además, el resultado buscará coincidencias parecidas a su búsqueda.<br>
                 Para buscar coincidencias exactas existe la posibilidad de buscar el texto entre comillas "".</div>
             @csrf
@@ -50,7 +93,6 @@
                     class="fa-solid fa-magnifying-glass"></i></button>
         </form>
     </div>
-
 </div>
 
 @endsection
