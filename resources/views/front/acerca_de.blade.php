55e00aff1928f4f888790ef7b855f8fb2a7a77e3
Juan B <juan.baronviciana@gmail.com>
2023-04-28 10:00:28 +0200

    Update acerca_de.blade.php

--- resources/views/front/acerca_de.blade.php
+++ resources/views/front/acerca_de.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.front')
 @section('content')
-<div class="container">
+<div class="container" style="margin-top: 1.5in">
     {!! $opciones['acerca_de_contenido'] !!}
 </div>
 @endsection
