e06c7e85adac50cfa8e0d42f2df2e367aab3c6cb
adolfo007123 <adolfocod00@gmail.com>
2023-05-05 09:12:57 +0200

    Añadido estilo a los parrafos de acerca_de.blade.php

--- resources/views/front/acerca_de.blade.php
+++ resources/views/front/acerca_de.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.front')
 @section('content')
+<style>
+    p{
+      text-align: justify;
+    }
+</style>
+  
 <div class="container" style="margin-top: 1.5in">
     {!! $opciones['acerca_de_contenido'] !!}
 </div>
