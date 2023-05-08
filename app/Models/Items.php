67bbc25f73c7b3e7c3d10e5eafee6994b99182ac
avozme <avozme@yahoo.com>
2023-05-08 11:12:03 +0200

    Resuelvo parcialmente issue #79: ya no salen items destacados duplicados por culpa de las etiquetas HTML ocultas

--- app/Models/Items.php
+++ app/Models/Items.php
@@ -33,6 +33,12 @@ class Items extends Model
     // Recupera todos los valores de un ítem
     public static function recuperarValores($idItem) {
         $valores = DB::select(DB::raw("SELECT value FROM items_productos WHERE items_id = '$idItem' GROUP BY value ORDER BY value"));
+        // Limpiamos los valores obtenidos de cualquier posible etiqueta HTML
+        foreach ($valores as $key => $value) {
+            $valores[$key]->value = strip_tags($value->value);
+        }
+        // Quitamos los elementos del array que pudieran estar repetidos después de haber quitado las etiquetas HTML
+        $valores = array_unique($valores, SORT_REGULAR);
         return $valores;
 
     }
