c94839a58728f5121249d4af0fafa182bbd9a89e
adolfo007123 <adolfocod00@gmail.com>
2023-05-26 10:04:01 +0200

    Added strip_tags sql function to a migration

--- database/migrations/2022_12_05_105207_create_table_items_productos.php
+++ database/migrations/2022_12_05_105207_create_table_items_productos.php
@@ -23,6 +23,23 @@ return new class extends Migration
 
             $table->timestamps();
         });
+
+        DB::unprepared('
+            CREATE FUNCTION strip_tags(str text) 
+            RETURNS text
+            DETERMINISTIC
+            BEGIN
+                DECLARE start, end INT DEFAULT 1;
+                LOOP
+                    SET start = LOCATE("<", str, start);
+                    IF (!start) THEN RETURN REPLACE(str, "&nbsp;", ""); END IF;
+                    SET end = LOCATE(">", str, start);
+                    IF (!end) THEN SET end = start; END IF;
+                    SET str = INSERT(str, start, end - start + 1, "");
+                    SET str = REPLACE(str, "&nbsp;", "");
+                END LOOP;
+            END'
+        );
     }
 
 
@@ -34,5 +51,6 @@ return new class extends Migration
     public function down()
     {
         Schema::dropIfExists('items_productos');
+        DB::unprepared('DROP FUNCTION IF EXISTS strip_tags');
     }
 };
